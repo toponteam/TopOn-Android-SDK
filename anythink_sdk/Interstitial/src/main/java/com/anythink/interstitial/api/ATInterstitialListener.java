4afcdda7fdb9b6d839dba38c8dfc9ea2451d6d25
zhangshiren <285995746@qq.com>
2020-11-12 17:22:26 +0800

    Update ATInterstitialListener.java

--- anythink_sdk/Interstitial/src/main/java/com/anythink/interstitial/api/ATInterstitialListener.java
+++ anythink_sdk/Interstitial/src/main/java/com/anythink/interstitial/api/ATInterstitialListener.java
@@ -4,13 +4,7 @@ import com.anythink.core.api.AdError;
 import com.anythink.core.api.ATAdInfo;
 
 
-/**
- * Copyright (C) 2018 {XX} Science and Technology Co., Ltd.
- *
- * @version V{2.3.0}
- * @Author ：Created by zhoushubin on 2018/9/19.
- * @Email: zhoushubin@salmonads.com
- */
+
 public interface ATInterstitialListener {
     public void onInterstitialAdLoaded();
 
